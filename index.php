a6c1d21f033040e4633317b7960e5f8cd60fb5e2
Romain <RomainFrechic@outlook.fr>
2017-01-18 15:27:39 +0100

    create style.css et tab en semantic

--- index.php
+++ index.php
@@ -3,15 +3,15 @@ require('user.php');
 
 
 $date= date('d/m/y',time());
-$client1 = new Client('id','@',$date);
+$client1 = new Client('1','futer@hotmail.fr',$date);
 //var_dump($client1);
-$client2 = new Client('id','@',$date);
+$client2 = new Client('2','mindbrainintelligency@outlook.fr',$date);
 //var_dump($client1);
-return [
+$monTableau = [
 $client1,
 $client2 
 ];
-
+return $monTableau;
  
 
 ?>
--- tab.php
+++ tab.php
@@ -8,21 +8,39 @@ $pageUser=require('index.php');
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+		<link rel="stylesheet" href="/css/style.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.6/semantic.min.css">
 	<title>Formulaire Utilisateur</title>
 </head>
 <body>
-	
+	<div class="ui segment" >
+		<table class="ui celled striped table" >
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Email</th>
+					<th>Date</th>
+				</tr>
+				</thead>
+			<tbody> 
+			
 	<?php 
-    
-	foreach ($pageUser as $key => $value) {
-		print_r($key);
-		print_r($value);
-		return;
-	}
-	
 
-	
-   
-	?>
-</body>
-</html>
+	foreach ($pageUser as $key => $pageUser):
+
+		?>
+		
+				<tr>
+					<td> <?php  echo $pageUser->getID(); ?></td>
+					<td> <?php  echo $pageUser->getEmail(); ?></td>
+					<td> <?php  echo $pageUser->getDate(); ?></td>
+				</tr>
+
+			
+		<?php endforeach ?>
+		</tbody>
+		</table>
+		</div>
+	</body>
+	</html>
+
